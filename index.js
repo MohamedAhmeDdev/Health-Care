67e4084c97ef5a317d83b1ff76f1c25bddccb03a
Mohamed <ma07041705@gamel.com>
2022-05-10 15:54:24 +0300

    add a sidebar navigation and content page

---
+++ index.js
@@ -0,0 +1,14 @@
+let menu = document.getElementById("menu-icon")
+let side_menu = document.getElementById("side")
+let list = document.querySelectorAll(".links_name")
+let logo_name = document.getElementById("logo")
+
+
+menu.addEventListener("click", () =>
+{
+    side_menu.classList.toggle("active");
+})
+
+// menu.addEventListener("click", ()=>{
+//     list.classList.toggle("show")
+// })

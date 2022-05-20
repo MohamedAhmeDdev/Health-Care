7d2bc502d7e9b0581c65aa5fb95076967cdeed34
Mohamed <ma07041705@gamel.com>
2022-05-20 10:51:54 +0300

    made some changes  on the admin. i modified the navbar and where the contint will be

--- dashboard.php
+++ dashboard.php
@@ -5,57 +5,60 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DASHBOARD</title>
-    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href=" css./dashboard.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <nav id="side" class="side-bar" >
-        <div id="menu-icon">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
+    <div class="sidebar" id="side">
+    <div id="menu-icon">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 
-        <h3 id="logo" class="logo">Health Care</h3>
-        <ul id="nav-list">
-           <li>
-            <a href="dashboard.php">
-                <i class="bi bi-speedometer2"></i>
-                <span class="links_name">DASHBOARD</span>
-            </a>
-           </li>
-           <li>
-            <a href="userinfo.php">
-                <i class="bi bi-person-circle"></i>
-                <span class="links_name">USERS</span>
-            </a>
-           </li>
-           <li>
-            <a href="">
-                <i class="bi bi-person-dash"></i>
-                <span class="links_name">ABOUT</span>
-            </a>
-           </li>
-           <li>
-            <a href="">
-                <i class="bi bi-question-circle"></i>
-                <span class="links_name">HELP</span>
-            </a>
-           </li>
-           <li>
-            <a href="settings.php">
-                <i class="bi bi-gear"></i>
-                <span class="links_name">SETINGS</span>
-            </a>
-           </li>
-        </ul>
-    </nav>
-    <section class="dashboard container-fluid">
-   </section>
+    <h3 id="logo" class="logo">Health Care</h3>
+    <ul id="nav-list">
+       <li>
+        <a href="dashboard.php">
+            <i class="bi bi-speedometer2"></i>
+            <span class="links_name">DASHBOARD</span>
+        </a>
+       </li>
+       <li>
+        <a href="userinfo.php">
+            <i class="bi bi-person-circle"></i>
+            <span class="links_name">USERS</span>
+        </a>
+       </li>
+       <li>
+        <a href="">
+            <i class="bi bi-person-dash"></i>
+            <span class="links_name">ABOUT</span>
+        </a>
+       </li>
+       <li>
+        <a href="">
+            <i class="bi bi-question-circle"></i>
+            <span class="links_name">HELP</span>
+        </a>
+       </li>
+       <li>
+        <a href="settings.php">
+            <i class="bi bi-gear"></i>
+            <span class="links_name">SETINGS</span>
+        </a>
+       </li>
+    </ul>
+</div>
+      <section class="home-section">
+          <div class="text">Dashboar</div>
+      </section>
+
     <script src="index.js"></script>
 </body>
 </html>
+
 
 
 

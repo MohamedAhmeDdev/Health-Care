a592e2a237a2412ea982bcab0024f37bb191bec2
Mohamed <ma07041705@gamel.com>
2022-06-03 21:28:07 +0300

    i have decided to remove all the backend (PHP) for now

--- db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO("mysql:localhost;port=3306;dbname=healthcare",'root','');
-$pdo->setAttribute(PDO::ATTR_ERRMODE,
-PDO::ERRMODE_EXCEPTION);
-?>
--- index.js
+++ index.js
@@ -4,37 +4,53 @@ let list = document.querySelectorAll(".links_name")
 let logo_name = document.getElementById("logo")
 
 
+
 menu.addEventListener("click", () =>
 {
     side_menu.classList.toggle("active");
 })
 
 
-// // form validation
+
+//   changing the  theme  color
+function colorTheme(){
+let check = document.getElementById("checkbox")
+
+check.addEventListener("change" ,() => {
+    document.body.classList.toggle("dark")
+})
+}
+colorTheme()
+
+
+//  form validation
 
 const nam = document.getElementById('name')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
-const errorElement = document.getElementById('error')
+let form = document.getElementById("creatAccount")
 
-document.getElementById("creatAccount").addEventListener( "submit", (e) => {
+
+
+form.addEventListener( "submit", (e) => {
     e.preventDefault()
-    let messages = []
-    if (nam.value === " " && nam.value === null) {
-     messages.push('Name is required') 
-    
+    if(nam.value == "" || nam.value.length <10) {
+        nameValidation = "Name Is Required"  
+    // alert("name")
     }
-  
-    if (password.value.length <= 6 && password.value.length >= 10){
-      messages.push('Password must be at list 16 characters')
+    else if(email.value == "" ){
+        // alert("email")
+        emailValidation = "Enter Email Address"
     }
-  
-    if (password.value === 12345) {
-      messages.push('Password cannot be password')
+    else if(password.value.length <= 6 ){
+        // alert("password)
+        passwordValidation = "Password must be at list 16 characters"
     }
-  
-    if (messages.length > 0) {
-      
-      errorElement.innerText = messages.join(', ')
+    else{
+        alert("welcome")
     }
+    document.querySelector(".errorName").innerHTML = nameValidation;
+    document.querySelector(".errorEmail").innerHTML =  emailValidation;
+    document.querySelector(".errorPassword").innerHTML =  passwordValidation;
   })
+
